e1cf495aa2cff651e24ca616e03d66279c00e6d3
Leolainen <leolainen2@gmail.com>
2020-08-12 18:31:56 +0200

    [code] split code into chunks and import what is needed. save last selected color in figma pluginData

--- src/code.ts
+++ src/code.ts
@@ -1,22 +1,27 @@
-import { COLORKEYS } from "./constants";
-import {
-  HSLToHex,
-  RGBToHex
-} from './converters/toHex';
+import { COLORKEYS, DEFAULT_BASE_COLOR } from "./constants";
 import {
   hexToRGB
 } from './converters/toRgb';
 import { hexToHSL } from './converters/toHsl';
 import clone from './utils/clone';
-import { RgbHslHexObject, BaseColorList } from "./types";
-import { generateMaterialPalette } from './generators/material/index';
+import { handleTextNodeContrast } from './utils/contrast';
+import { RgbHslHexObject } from "./types";
+import { generateMaterialPalette } from './generators/material';
+import { generateMonochromePalette } from './generators/monochrome';
 
 figma.showUI(__html__, {
-  height: 500
+  height: 800,
+  width: 900
+});
+
+figma.ui.postMessage({
+  lastSelectedColor: figma.root.getPluginData('lastSelectedColor') || DEFAULT_BASE_COLOR
 });
 
 figma.ui.onmessage = async msg => {
   if (msg.type === "create-palette") {
+    figma.root.setPluginData('lastSelectedColor', msg.value);
+
     const nodes: SceneNode[] = [];
 
     let selectedColor: string = msg.value[0] === "#" ? msg.value : `#${msg.value}`;
@@ -153,151 +158,3 @@ figma.ui.onmessage = async msg => {
 
   figma.closePlugin();
 };
-
-function generateMonochromePalette(
-  baseColor: RgbHslHexObject,
-  trueMonochrome: Boolean = false
-) {
-  const brighterColors: RgbHslHexObject[] = createBrighterColors(
-    baseColor
-  ).reverse();
-  const darkerColors: RgbHslHexObject[] = trueMonochrome
-    ? createDarkerColors(baseColor, 4, 0)
-    : createDarkerColors(baseColor);
-  const palette: RgbHslHexObject[] = [
-    ...brighterColors,
-    baseColor,
-    ...darkerColors
-  ];
-
-  return palette;
-}
-
-
-/**
- *
- * @param {TextNode} text – Figma textNode object
- * @param {string} backgroundColor – CSS Hex color. Ex: #440044
- *
- * returns typeof symbol
- */
-function handleTextNodeContrast(text: TextNode, backgroundColor: string) {
-  // Get contrast ratio to set text color
-  const textRGB = text.fills[0].color;
-  const textHex = RGBToHex(textRGB);
-  const contrastRatio = getContrastRatio(textHex, backgroundColor);
-  const textFills = clone(text.fills);
-
-  // Sets text color if contrast is too low
-  if (contrastRatio < 6) {
-    textFills[0].color.r = 1;
-    textFills[0].color.g = 1;
-    textFills[0].color.b = 1;
-  }
-
-  return textFills;
-}
-
-
-function createBrighterColors(baseColor: RgbHslHexObject, length: number = 5) {
-  const maxSaturation = 100;
-  const maxLightness = 100;
-  let sModHolder = 0;
-  let lModHolder = 0;
-
-  return Array.from(new Array(length), () => {
-    const { h, s, l } = baseColor.hsl;
-
-    if (sModHolder === 0 && lModHolder === 0) {
-      const sDiff = maxSaturation - s;
-      const lDiff = maxLightness - l;
-
-      sModHolder = s + sDiff * 0.05;
-      lModHolder = l + lDiff * 0.25;
-    } else {
-      const sDiff = maxSaturation - sModHolder;
-      const lDiff = maxLightness - lModHolder;
-
-      sModHolder = sModHolder + sDiff * 0.05;
-      lModHolder = lModHolder + lDiff * 0.25;
-    }
-
-    const hex = HSLToHex(h, s, lModHolder);
-
-    return {
-      rgb: hexToRGB(hex, true),
-      hsl: hexToHSL(hex),
-      hex
-    };
-  });
-}
-
-function createDarkerColors(
-  baseColor: RgbHslHexObject,
-  length: number = 4,
-  hModMultiplier: number = 0.02
-) {
-  let sModHolder = 0;
-  let lModHolder = 0;
-  let hModHolder = 0;
-
-  return Array.from(new Array(length), () => {
-    const { h, s, l } = baseColor.hsl;
-
-    if (sModHolder === 0 && lModHolder === 0) {
-      sModHolder = s - s * 0.2;
-      lModHolder = l - l * 0.15;
-      hModHolder = h - h * hModMultiplier < 0 ? 360 : h - h * hModMultiplier;
-    } else {
-      sModHolder = sModHolder - sModHolder * 0.1;
-      lModHolder = lModHolder - lModHolder * 0.15;
-      hModHolder =
-        hModHolder - hModHolder * hModMultiplier < 0
-          ? 360
-          : hModHolder - hModHolder * hModMultiplier;
-    }
-
-    const hex = HSLToHex(hModHolder, sModHolder, lModHolder);
-
-    return {
-      rgb: hexToRGB(hex, true),
-      hsl: hexToHSL(hex),
-      hex
-    };
-  });
-}
-
-/**
- * Calculates the contrast ratio between two colors.
- * Formula: https://www.w3.org/TR/WCAG20-TECHS/G17.html#G17-tests
- *
- * @param {string} foreground - ex: #F0463C
- * @param {string} background - ex: #212126
- * @returns {number} A contrast ratio value in the range 0 - 21.
- */
-function getContrastRatio(foreground: string, background: string) {
-  const lumA = getLuminance(foreground);
-  const lumB = getLuminance(background);
-  return (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
-}
-
-// Functions picked and adjusted from
-// https://github.com/mui-org/material-ui/blob/master/packages/material-ui/src/styles/colorManipulator.js
-/**
- * The relative brightness of any point in a color space,
- * normalized to 0 for darkest black and 1 for lightest white.
- * @param {string} hex – ex: #F0463C
- */
-function getLuminance(hex: string) {
-  const rgb = hexToRGB(hex);
-
-  const rgbVal = [rgb.r, rgb.g, rgb.b].map(val => {
-    val /= 255; // normalized
-    return val <= 0.03928 ? val / 12.92 : ((val + 0.055) / 1.055) ** 2.4;
-  });
-
-  // Truncate at 3 digits
-  return Number(
-    (0.2126 * rgbVal[0] + 0.7152 * rgbVal[1] + 0.0722 * rgbVal[2]).toFixed(3)
-  );
-}
