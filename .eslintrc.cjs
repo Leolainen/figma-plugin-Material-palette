7b495338b64ab9169d778964ed5a68e16f0ca7f3
Leolainen <leolainen2@gmail.com>
2022-04-15 12:31:15 +0200

    [package] move supposed dev dependencies to devdependencies [package] uninstall unused and bloat packages [eslint] reinstall everything from scratch. extend google rules [typesript] reinstall from scratch + new config

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "google", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+  overrides: [
+    {
+      files: ["*.js", "*.cjs", "*.ts", "*.tsx"],
+      rules: {
+        quotes: ["warn", "double"],
+      },
+    },
+  ],
+};
