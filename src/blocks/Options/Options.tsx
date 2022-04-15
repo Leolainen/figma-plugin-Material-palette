ffbe9bad07a8b0627c9932048e6f9d57a17ac598
Leolainen <leolainen2@gmail.com>
2022-04-15 13:34:34 +0200

    [Options] fix TS errors

--- src/blocks/Options/Options.tsx
+++ src/blocks/Options/Options.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import { IconButton, Popover, Tooltip } from "@mui/material";
 import { Settings as SettingsIcon } from "@mui/icons-material";
 import Toggle from "../../components/Toggle";
@@ -25,16 +25,18 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const Options: React.FC<Props> = ({
+const Options = ({
   className,
   onOptionsChange,
   options,
   optionsDisabled,
-}) => {
+}: Props) => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
 
-  const handleClick = (event) => {
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
     if (event.currentTarget) {
       setAnchorEl(event.currentTarget);
     }
@@ -56,46 +58,48 @@ const Options: React.FC<Props> = ({
     [onOptionsChange, options]
   );
 
-  return <>
-    <Tooltip title="Options" placement="bottom">
-      <IconButton className={className} onClick={handleClick} size="large">
-        <SettingsIcon />
-      </IconButton>
-    </Tooltip>
+  return (
+    <>
+      <Tooltip title="Options" placement="bottom">
+        <IconButton className={className} onClick={handleClick} size="large">
+          <SettingsIcon />
+        </IconButton>
+      </Tooltip>
 
-    <Popover
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: "center",
-        horizontal: "right",
-      }}
-      classes={{ paper: classes.popover }}
-      onClose={handleClose}
-      open={open}
-      transformOrigin={{
-        vertical: "center",
-        horizontal: "left",
-      }}
-    >
-      <Toggle
-        checked={options.accent}
-        onChange={handleChange}
-        name="accent"
-        disabled={optionsDisabled.accent}
-        label="Accent colors"
-        hint="Toggles the accent colors on or off (material only)"
-      />
+      <Popover
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "right",
+        }}
+        classes={{ paper: classes.popover }}
+        onClose={handleClose}
+        open={open}
+        transformOrigin={{
+          vertical: "center",
+          horizontal: "left",
+        }}
+      >
+        <Toggle
+          checked={options.accent}
+          onChange={handleChange}
+          name="accent"
+          disabled={optionsDisabled.accent}
+          label="Accent colors"
+          hint="Toggles the accent colors on or off (material only)"
+        />
 
-      <Toggle
-        checked={options.lockSwatch}
-        onChange={handleChange}
-        name="lockSwatch"
-        disabled={optionsDisabled.lockSwatch}
-        label="Lock swatch"
-        hint="Locks the input value to swatch 500 when toggled on (material only)"
-      />
-    </Popover>
-  </>;
+        <Toggle
+          checked={options.lockSwatch}
+          onChange={handleChange}
+          name="lockSwatch"
+          disabled={optionsDisabled.lockSwatch}
+          label="Lock swatch"
+          hint="Locks the input value to swatch 500 when toggled on (material only)"
+        />
+      </Popover>
+    </>
+  );
 };
 
 export default Options;
