17655c551be4112c3461da3b7b04cc852900405f
Leolainen <leolainen2@gmail.com>
2022-04-27 00:15:19 +0200

    [ColorPicker] rebuilt to render a confirm & dismiss button

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,31 +1,102 @@
 import * as React from "react";
-import { Popover, PopoverProps } from "@mui/material";
-import { ChromePicker, ColorChangeHandler } from "react-color";
+import Button, { ButtonProps } from "@mui/material/Button";
+import Popover, { PopoverProps } from "@mui/material/Popover";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import Stack from "@mui/material/Stack";
+import { ChromePicker, ColorChangeHandler, ColorResult } from "react-color";
 
 export interface Props {
-  onChange: ColorChangeHandler;
+  onConfirm: (color: ColorResult) => void;
   onClose: PopoverProps["onClose"];
+  onChange?: ColorChangeHandler;
   anchorEl: PopoverProps["anchorEl"];
   open: PopoverProps["open"];
   value: string;
 }
 
-const ColorPicker = ({ onChange, anchorEl, open, value, onClose }: Props) => (
-  <Popover
-    anchorEl={anchorEl}
-    open={open}
-    onClose={onClose}
-    anchorOrigin={{
-      vertical: "center",
-      horizontal: "right",
-    }}
-    transformOrigin={{
-      vertical: "center",
-      horizontal: "right",
-    }}
-  >
-    <ChromePicker onChange={onChange} color={value} />
-  </Popover>
-);
+const ColorPicker = ({
+  onConfirm,
+  onChange,
+  anchorEl,
+  open,
+  value,
+  onClose,
+}: Props) => {
+  const [selectedColor, setSelectedColor] = React.useState<ColorResult>();
+
+  const handleChange: ColorChangeHandler = (color, event) => {
+    setSelectedColor(color);
+
+    if (onChange) {
+      onChange(color, event);
+    }
+  };
+
+  const handleConfirm = () => {
+    if (!selectedColor) return;
+
+    onConfirm(selectedColor);
+  };
+
+  const handleClose: ButtonProps["onClick"] = (event) => {
+    if (!onClose) return;
+
+    onClose(event, "backdropClick"); // "reason" arg just to satisfy typescript
+  };
+
+  return (
+    <Popover
+      anchorEl={anchorEl}
+      open={open}
+      onClose={onClose}
+      anchorOrigin={{
+        vertical: "center",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "center",
+        horizontal: "right",
+      }}
+      sx={{
+        p: 1,
+      }}
+    >
+      <ChromePicker
+        onChange={handleChange}
+        color={selectedColor?.hex || value}
+        styles={{
+          default: {
+            picker: {
+              boxShadow: "none",
+            },
+            body: {
+              boxShadow: "none",
+            },
+          },
+        }}
+      />
+
+      <Stack direction="row">
+        <Button
+          startIcon={<CloseIcon />}
+          color="error"
+          onClick={handleClose}
+          fullWidth
+        >
+          Cancel
+        </Button>
+        <Button
+          startIcon={<CheckIcon />}
+          color="success"
+          onClick={handleConfirm}
+          fullWidth
+        >
+          Confirm
+        </Button>
+      </Stack>
+    </Popover>
+  );
+};
 
 export default ColorPicker;
