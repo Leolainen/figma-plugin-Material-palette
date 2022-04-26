fda265843d477f2fd77b5c80d1886b3c92776f62
Leolainen <leolainen2@gmail.com>
2022-04-27 00:33:34 +0200

    [Preview] rebuilt with MUI5 syntax & handle ColorPicker confirm/dismiss

--- src/blocks/Preview/Preview.tsx
+++ src/blocks/Preview/Preview.tsx
@@ -1,86 +1,39 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import * as React from "react";
-import makeStyles from "@mui/styles/makeStyles";
-import { ButtonBase, Typography } from "@mui/material";
-import classnames from "classnames";
-import { ColorChangeHandler } from "react-color";
+import { ColorChangeHandler, ColorResult } from "react-color";
+import ColorBar from "../../components/ColorBar";
+import ColorPicker from "../../components/ColorPicker";
 import { Palette } from "../../types";
 import { handleTextContrast } from "../../utils";
-import ColorPicker from "../../components/ColorPicker";
 
 interface Props {
   colorValue: string;
   paletteName: string;
   preview: Palette;
   onPaletteChange: (palette: Palette) => void;
-  style?: React.CSSProperties;
 }
-
-const useStyles = makeStyles(() => ({
-  previewScaled: {
-    transform: "scale(0.7)",
-    transformOrigin: "top",
-  },
-
-  previewSwatchHeader: {
-    height: 102,
-    width: 360,
-    display: "flex",
-    flexDirection: "column",
-    padding: 20,
-
-    "& > div": {
-      marginTop: "auto",
-      display: "flex",
-      justifyContent: "space-between",
-      textTransform: "uppercase",
-    },
-  },
-
-  previewSwatch: {
-    width: 360,
-    height: 14,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 20,
-    textTransform: "uppercase",
-    cursor: "pointer",
-
-    p: {
-      fontSize: 18,
-    },
-
-    "&:hover": {
-      outline: "3px solid hsla(150, 60%, 50%, 0.7)",
-    },
-  },
-
-  mainSwatch: {
-    border: "2px dashed white",
-    padding: 18,
-  },
-}));
 
 const Preview = ({
   colorValue,
   paletteName,
   preview,
-  style,
   onPaletteChange,
-  ...props
 }: Props) => {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [selectedColor, setSelectedColor] =
     React.useState<[keyof Palette, string]>();
 
-  const handleSwatchClick =
-    (swatch: [string, any]) => (event: React.MouseEvent<HTMLButtonElement>) => {
-      setSelectedColor(swatch as typeof selectedColor);
-      setAnchorEl(event.currentTarget);
-    };
+  const handleSwatchClick = (
+    swatch: [string, any],
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setSelectedColor(swatch as typeof selectedColor);
+    setAnchorEl(event.currentTarget);
+  };
 
-  const handleColorChange: ColorChangeHandler = (color) => {
+  const handleColorChange: ColorChangeHandler = (color: ColorResult, event) => {
     if (!selectedColor) return;
 
     const [swatch, hex] = selectedColor;
@@ -93,6 +46,29 @@ const Preview = ({
     const newPalette: Palette = { ...preview };
     newPalette[swatch] = color.hex;
 
+    anchorEl?.setAttribute("style", "background-color: " + color.hex);
+  };
+
+  const handleColorPickerClose = () => {
+    anchorEl?.removeAttribute("style");
+    setAnchorEl(null);
+  };
+
+  const handleColorConfirm = (color: ColorResult) => {
+    if (!selectedColor) return;
+
+    const [swatch, hex] = selectedColor;
+
+    if (!swatch || !hex) {
+      console.error("no swatch or hex was found");
+      return;
+    }
+
+    const newPalette: Palette = { ...preview };
+    newPalette[swatch] = color.hex;
+
+    anchorEl?.removeAttribute("style");
+
     onPaletteChange(newPalette);
     setAnchorEl(null);
   };
@@ -102,65 +78,63 @@ const Preview = ({
   }
 
   return (
-    <div className={classes.previewScaled} style={style} {...props}>
-      <div
-        className={classes.previewSwatchHeader}
-        style={{
-          color: handleTextContrast(preview["500"]),
-          backgroundColor: preview["500"],
+    <Box mx={2}>
+      <Box
+        sx={{
+          width: 360,
+          margin: "auto",
         }}
       >
-        <Typography variant="body1">{paletteName}</Typography>
+        <Box
+          sx={{
+            height: 102,
+            display: "flex",
+            flexDirection: "column",
+            color: handleTextContrast(preview["500"]),
+            backgroundColor: preview["500"],
+          }}
+        >
+          <Typography px={2} pt={2}>
+            {paletteName}
+          </Typography>
 
-        <div>
-          <Typography variant="body1">500</Typography>
-
-          <Typography variant="body1">{preview["500"]}</Typography>
-        </div>
-      </div>
-
-      <ColorPicker
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        onClose={() => setAnchorEl(null)}
-        onChange={handleColorChange}
-        // value={Object.values(selectedSwatch.current)[0]}
-        value={(selectedColor && selectedColor[1]) || "#000000"}
-      />
-
-      {Object.entries(preview).map(([swatchKey, value], idx) => (
-        <React.Fragment key={swatchKey + value}>
-          <ButtonBase
-            className={classnames(classes.previewSwatch, {
-              [classes.mainSwatch]: colorValue === value,
-            })}
-            style={{
-              backgroundColor: value,
-            }}
-            key={`${colorValue}${idx}`}
-            onClick={handleSwatchClick([swatchKey, value])}
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            typography="button"
+            mt="auto"
+            px={2}
+            pb={2}
           >
-            <Typography
-              variant="body2"
-              style={{
-                color: handleTextContrast(value),
-              }}
-            >
-              {swatchKey}
-            </Typography>
+            <span>500</span>
+            <span>{preview["500"]}</span>
+          </Stack>
+        </Box>
 
-            <Typography
-              variant="body2"
-              style={{
-                color: handleTextContrast(value),
-              }}
-            >
-              {value}
-            </Typography>
-          </ButtonBase>
-        </React.Fragment>
-      ))}
-    </div>
+        <ColorPicker
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={handleColorPickerClose}
+          onConfirm={handleColorConfirm}
+          onChange={handleColorChange}
+          value={(selectedColor && selectedColor[1]) || "#000000"}
+        />
+
+        {Object.entries(preview).map(([swatchKey, value], idx) => (
+          <ColorBar
+            key={swatchKey + value}
+            swatch={[swatchKey as keyof Palette, value]}
+            onClick={handleSwatchClick}
+            sx={{
+              ...(value === colorValue && {
+                border: "2px dashed white",
+                padding: "18px",
+              }),
+            }}
+          />
+        ))}
+      </Box>
+    </Box>
   );
 };
 
