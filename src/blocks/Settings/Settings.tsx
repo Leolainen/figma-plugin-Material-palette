f8ab73a1f06a61ddf98cae57a0a6d6cd75cf5f1b
Leolainen <leolainen2@gmail.com>
2022-05-24 00:09:07 +0200

    [Settings] add confirmation dialog when resetting

--- src/blocks/Settings/Settings.tsx
+++ src/blocks/Settings/Settings.tsx
@@ -5,6 +5,11 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
 import FigmaSettings from "./partials/FigmaSettings";
 import GeneralSettings from "./partials/GeneralSettings";
 import { useAtom } from "jotai";
@@ -14,42 +19,63 @@ interface Props {}
 
 const Settings = React.forwardRef<HTMLUListElement, Props>((props, ref) => {
   const [, setSettings] = useAtom(atoms.settingsAtom);
+  const [showResetDialog, setShowResetDialog] = React.useState(false);
+
+  const openResetDialog = () => setShowResetDialog(true);
+  const closeResetDialog = () => setShowResetDialog(false);
 
   const handleReset = () => {
     setSettings(atoms.defaultSettings);
+    closeResetDialog();
   };
 
   return (
-    <Stack ref={ref}>
-      <List
-        dense
-        ref={ref}
-        sx={{
-          "& li > div:first-child": {
-            flex: "70%",
-            maxWidth: "65%",
-          },
-          "& li > *:last-child": {
-            maxWidth: "30%",
-            ml: "auto",
-          },
-        }}
-      >
-        <FigmaSettings />
-        <GeneralSettings />
+    <>
+      <Stack ref={ref}>
+        <List
+          dense
+          ref={ref}
+          sx={{
+            "& li > div:first-child": {
+              flex: "70%",
+              maxWidth: "65%",
+            },
+            "& li > *:last-child": {
+              maxWidth: "30%",
+              ml: "auto",
+            },
+          }}
+        >
+          <FigmaSettings />
+          <GeneralSettings />
 
-        <Divider />
+          <Divider />
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Reset settings"
-            secondary="Restore all settings back to default"
-          />
+          <ListItem disableGutters>
+            <ListItemText
+              primary="Reset settings"
+              secondary="Restore all settings back to default"
+            />
 
+            <Button onClick={openResetDialog}>Reset</Button>
+          </ListItem>
+        </List>
+      </Stack>
+
+      <Dialog open={showResetDialog} onClose={closeResetDialog}>
+        <DialogTitle>Reset settings</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to reset all settings back to default?
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={closeResetDialog}>Cancel</Button>
           <Button onClick={handleReset}>Reset</Button>
-        </ListItem>
-      </List>
-    </Stack>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 });
 
