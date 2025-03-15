686b38f9a97dc45f0fe509a3b090cfe42c6a68f5
Leolainen <leolainen2@gmail.com>
2025-03-15 16:25:26 +0100

    fix(ui.tsx): waits for DOMCountentLoaded before mounting react

--- src/ui.tsx
+++ src/ui.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 
-const container = document.getElementById("react-page");
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-
-root.render(<App />);
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("react-page");
+  const root = createRoot(container!);
+  root.render(<App />);
+});
