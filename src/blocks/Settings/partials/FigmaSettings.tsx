84f0b475dd763384f3f165ab67549d18e92073db
Leolainen <leolainen2@gmail.com>
2025-04-15 22:22:19 +0200

    fix(Settings): new styles + prettier

--- src/blocks/Settings/partials/FigmaSettings.tsx
+++ src/blocks/Settings/partials/FigmaSettings.tsx
@@ -8,94 +8,94 @@ import { useAtom } from "jotai";
 import * as atoms from "../../../store";
 import * as SettingsTypes from "../../../store/types/settings";
 
-interface Props {}
+interface Props { }
 
 const FigmaSettings = React.forwardRef<HTMLUListElement, Props>(
-  (props, ref) => {
-    const [renderWithOutline, setRenderWithOutline] = useAtom(
-      atoms.renderWithOutlineAtom
-    );
-    const [lock, setLock] = useAtom(atoms.lockAtom);
-    const [nodeType, setNodeType] = useAtom(atoms.nodeTypeAtom);
+    (props, ref) => {
+        const [renderWithOutline, setRenderWithOutline] = useAtom(
+            atoms.renderWithOutline
+        );
+        const [lock, setLock] = useAtom(atoms.lock);
+        const [nodeType, setNodeType] = useAtom(atoms.nodeType);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      let value;
+        const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            let value;
 
-      if (event.target.type === "checkbox") {
-        value = event.target.checked;
-      } else {
-        value = event.target.value;
-      }
+            if (event.target.type === "checkbox") {
+                value = event.target.checked;
+            } else {
+                value = event.target.value;
+            }
 
-      switch (event.target.name) {
-        case "renderWithOutline":
-          setRenderWithOutline(value as boolean);
-          break;
-        case "lock":
-          setLock(value as SettingsTypes.Lock);
-          break;
-        case "nodeType":
-          setNodeType(value as SettingsTypes.NodeType);
-          break;
-        default:
-          break;
-      }
-    };
+            switch (event.target.name) {
+                case "renderWithOutline":
+                    setRenderWithOutline(value as boolean);
+                    break;
+                case "lock":
+                    setLock(value as SettingsTypes.Lock);
+                    break;
+                case "nodeType":
+                    setNodeType(value as SettingsTypes.NodeType);
+                    break;
+                default:
+                    break;
+            }
+        };
 
-    return (
-      <>
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Node type"
-            secondary="The node type of the palette"
-          />
+        return (
+            <>
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Node type"
+                        secondary="The node type of the palette"
+                    />
 
-          <TextField
-            onChange={handleChange}
-            select
-            value={nodeType}
-            fullWidth
-            name="nodeType"
-          >
-            <MenuItem value="component">Component</MenuItem>
-            <MenuItem value="frame">Frame</MenuItem>
-          </TextField>
-        </ListItem>
+                    <TextField
+                        onChange={handleChange}
+                        select
+                        value={nodeType}
+                        fullWidth
+                        name="nodeType"
+                    >
+                        <MenuItem value="component">Component</MenuItem>
+                        <MenuItem value="frame">Frame</MenuItem>
+                    </TextField>
+                </ListItem>
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Lock"
-            secondary="The nodes that should be locked"
-          />
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Lock"
+                        secondary="The nodes that should be locked"
+                    />
 
-          <TextField
-            onChange={handleChange}
-            select
-            value={lock}
-            fullWidth
-            name="lock"
-          >
-            <MenuItem value="everything">Everything</MenuItem>
-            <MenuItem value="swatches">Swatches</MenuItem>
-            <MenuItem value="nothing">Nothing</MenuItem>
-          </TextField>
-        </ListItem>
+                    <TextField
+                        onChange={handleChange}
+                        select
+                        value={lock}
+                        fullWidth
+                        name="lock"
+                    >
+                        <MenuItem value="everything">Everything</MenuItem>
+                        <MenuItem value="swatches">Swatches</MenuItem>
+                        <MenuItem value="nothing">Nothing</MenuItem>
+                    </TextField>
+                </ListItem>
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Render with outlines"
-            secondary="Add an outline to the input color node"
-          />
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Render with outlines"
+                        secondary="Add an outline to the input color node"
+                    />
 
-          <Checkbox
-            onChange={handleChange}
-            name="renderWithOutline"
-            checked={renderWithOutline}
-          />
-        </ListItem>
-      </>
-    );
-  }
+                    <Checkbox
+                        onChange={handleChange}
+                        name="renderWithOutline"
+                        checked={renderWithOutline}
+                    />
+                </ListItem>
+            </>
+        );
+    }
 );
 
 FigmaSettings.displayName = "FigmaSettings";
--- src/blocks/Settings/partials/GeneralSettings.tsx
+++ src/blocks/Settings/partials/GeneralSettings.tsx
@@ -8,54 +8,54 @@ import { useAtom } from "jotai";
 import * as atoms from "../../../store";
 import * as SettingsTypes from "../../../store/types/settings";
 
-interface Props {}
+interface Props { }
 
 const GeneralSettings = React.forwardRef<HTMLUListElement, Props>(
-  (props, ref) => {
-    const [paletteDirection, setPaletteDirection] = useAtom(
-      atoms.paletteDirectionAtom
-    );
-    const [colorBarWidth, setColorBarWidth] = useAtom(atoms.colorBarWidthAtom);
-    const [colorBarHeight, setColorBarHeight] = useAtom(
-      atoms.colorBarHeightAtom
-    );
-    const [header, setHeader] = useAtom(atoms.headerAtom);
+    (props, ref) => {
+        const [paletteDirection, setPaletteDirection] = useAtom(
+            atoms.paletteDirection
+        );
+        const [colorBarWidth, setColorBarWidth] = useAtom(atoms.colorBarWidth);
+        const [colorBarHeight, setColorBarHeight] = useAtom(
+            atoms.colorBarHeight
+        );
+        const [header, setHeader] = useAtom(atoms.header);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      let value;
+        const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            let value;
 
-      if (event.target.type === "checkbox") {
-        value = event.target.checked;
-      } else {
-        // fix bug with rect resizing in figma
-        if (["colorBarWidth", "colorBarHeight"].includes(event.target.name)) {
-          value = parseInt(event.target.value, 10);
-        } else {
-          value = event.target.value;
-        }
-      }
+            if (event.target.type === "checkbox") {
+                value = event.target.checked;
+            } else {
+                // fix bug with rect resizing in figma
+                if (["colorBarWidth", "colorBarHeight"].includes(event.target.name)) {
+                    value = parseInt(event.target.value, 10);
+                } else {
+                    value = event.target.value;
+                }
+            }
 
-      switch (event.target.name) {
-        case "paletteDirection":
-          setPaletteDirection(value as SettingsTypes.PaletteDirection);
-          break;
-        case "colorBarWidth":
-          setColorBarWidth(value as number);
-          break;
-        case "colorBarHeight":
-          setColorBarHeight(value as number);
-          break;
-        case "header":
-          setHeader(value as boolean);
-          break;
-        default:
-          break;
-      }
-    };
+            switch (event.target.name) {
+                case "paletteDirection":
+                    setPaletteDirection(value as SettingsTypes.PaletteDirection);
+                    break;
+                case "colorBarWidth":
+                    setColorBarWidth(value as number);
+                    break;
+                case "colorBarHeight":
+                    setColorBarHeight(value as number);
+                    break;
+                case "header":
+                    setHeader(value as boolean);
+                    break;
+                default:
+                    break;
+            }
+        };
 
-    return (
-      <>
-        {/* 
+        return (
+            <>
+                {/* 
         <ListItem disableGutters>
           <ListItemText
             primary="Presets"
@@ -75,70 +75,70 @@ const GeneralSettings = React.forwardRef<HTMLUListElement, Props>(
           </TextField>
         </ListItem> */}
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Direction"
-            secondary="Arrange colors in row or column"
-          />
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Direction"
+                        secondary="Arrange colors in row or column"
+                    />
 
-          <TextField
-            select
-            value={paletteDirection}
-            fullWidth
-            name="paletteDirection"
-            onChange={handleChange}
-          >
-            <MenuItem value="row">Row</MenuItem>
-            <MenuItem value="column">Column</MenuItem>
-          </TextField>
-        </ListItem>
+                    <TextField
+                        select
+                        value={paletteDirection}
+                        fullWidth
+                        name="paletteDirection"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value="row">Row</MenuItem>
+                        <MenuItem value="column">Column</MenuItem>
+                    </TextField>
+                </ListItem>
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Swatch width"
-            secondary="The width of the swatches in pixels"
-          />
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Swatch width"
+                        secondary="The width of the swatches in pixels"
+                    />
 
-          <TextField
-            placeholder="swatch width"
-            type="number"
-            value={colorBarWidth}
-            name="colorBarWidth"
-            onChange={handleChange}
-          />
-        </ListItem>
+                    <TextField
+                        placeholder="swatch width"
+                        type="number"
+                        value={colorBarWidth}
+                        name="colorBarWidth"
+                        onChange={handleChange}
+                    />
+                </ListItem>
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Swatch height"
-            secondary="The height of the swatches in pixels"
-          />
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Swatch height"
+                        secondary="The height of the swatches in pixels"
+                    />
 
-          <TextField
-            placeholder="swatch height"
-            type="number"
-            value={colorBarHeight}
-            name="colorBarHeight"
-            onChange={handleChange}
-          />
-        </ListItem>
+                    <TextField
+                        placeholder="swatch height"
+                        type="number"
+                        value={colorBarHeight}
+                        name="colorBarHeight"
+                        onChange={handleChange}
+                    />
+                </ListItem>
 
-        <ListItem disableGutters>
-          <ListItemText
-            primary="Header color"
-            secondary="Toggle the header color"
-          />
+                <ListItem disableGutters>
+                    <ListItemText
+                        primary="Header color"
+                        secondary="Toggle the header color"
+                    />
 
-          <Switch
-            edge="end"
-            onChange={handleChange}
-            name="header"
-            checked={header}
-          />
-        </ListItem>
-      </>
-    );
-  }
+                    <Switch
+                        edge="end"
+                        onChange={handleChange}
+                        name="header"
+                        checked={header}
+                    />
+                </ListItem>
+            </>
+        );
+    }
 );
 
 GeneralSettings.displayName = "GeneralSettings";
